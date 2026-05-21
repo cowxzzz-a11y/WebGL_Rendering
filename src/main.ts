70b83fddc70db1a54a5fea69ed76de6542098984
kaozhechi2013 <291938344@qq.com>
2026-05-21 09:20:34 +0800

    新增

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import '@babylonjs/core/Culling/ray'
 import '@babylonjs/core/Materials/Textures/Loaders/envTextureLoader'
 import '@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/defaultRenderingPipeline'
 import '@babylonjs/loaders/glTF'
+import * as KTX2DECODER from '@babylonjs/ktx2decoder'
 import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera'
 import { Engine } from '@babylonjs/core/Engines/engine'
 import { DirectionalLight } from '@babylonjs/core/Lights/directionalLight'
@@ -16,11 +17,21 @@ import { AbstractMesh } from '@babylonjs/core/Meshes/abstractMesh'
 import { LinesMesh } from '@babylonjs/core/Meshes/linesMesh'
 import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder'
 import { TransformNode } from '@babylonjs/core/Meshes/transformNode'
+import { KhronosTextureContainer2 } from '@babylonjs/core/Misc/khronosTextureContainer2'
 import { DefaultRenderingPipeline } from '@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/defaultRenderingPipeline'
 import { ImportMeshAsync } from '@babylonjs/core/Loading/sceneLoader'
 import { Scene } from '@babylonjs/core/scene'
 import { configStorageKey } from './viewerConfig'
 import type { ColorConfig, VectorConfig, ViewerConfig } from './viewerConfig'
+import mscBasisTranscoderJsUrl from '@babylonjs/ktx2decoder/wasm/msc_basis_transcoder.js?url'
+import mscBasisTranscoderWasmUrl from '@babylonjs/ktx2decoder/wasm/msc_basis_transcoder.wasm?url'
+import uastcAstcWasmUrl from '@babylonjs/ktx2decoder/wasm/uastc_astc.wasm?url'
+import uastcBc7WasmUrl from '@babylonjs/ktx2decoder/wasm/uastc_bc7.wasm?url'
+import uastcR8WasmUrl from '@babylonjs/ktx2decoder/wasm/uastc_r8_unorm.wasm?url'
+import uastcRg8WasmUrl from '@babylonjs/ktx2decoder/wasm/uastc_rg8_unorm.wasm?url'
+import uastcRgbaSrgbWasmUrl from '@babylonjs/ktx2decoder/wasm/uastc_rgba8_srgb_v2.wasm?url'
+import uastcRgbaUnormWasmUrl from '@babylonjs/ktx2decoder/wasm/uastc_rgba8_unorm_v2.wasm?url'
+import zstdWasmUrl from '@babylonjs/ktx2decoder/wasm/zstddec.wasm?url'
 
 type OutlineNode = {
   name: string
@@ -117,6 +128,24 @@ const defaultModels: DefaultModel[] = Object.entries(defaultModelUrls)
     fileName: path.replace(/^\.\.\/assets\//, 'assets/'),
   }))
 const environmentUrl = '/environment.env'
+
+const configureLocalKtx2Decoder = () => {
+  KTX2DECODER.LiteTranscoder_UASTC_ASTC.WasmModuleURL = uastcAstcWasmUrl
+  KTX2DECODER.LiteTranscoder_UASTC_BC7.WasmModuleURL = uastcBc7WasmUrl
+  KTX2DECODER.LiteTranscoder_UASTC_R8_UNORM.WasmModuleURL = uastcR8WasmUrl
+  KTX2DECODER.LiteTranscoder_UASTC_RG8_UNORM.WasmModuleURL = uastcRg8WasmUrl
+  KTX2DECODER.LiteTranscoder_UASTC_RGBA_SRGB.WasmModuleURL = uastcRgbaSrgbWasmUrl
+  KTX2DECODER.LiteTranscoder_UASTC_RGBA_UNORM.WasmModuleURL = uastcRgbaUnormWasmUrl
+  KTX2DECODER.MSCTranscoder.JSModuleURL = mscBasisTranscoderJsUrl
+  KTX2DECODER.MSCTranscoder.WasmModuleURL = mscBasisTranscoderWasmUrl
+  KTX2DECODER.ZSTDDecoder.WasmModuleURL = zstdWasmUrl
+
+  // Keep KTX2 decoding inside the app bundle instead of runtime CDN fetches.
+  ;(globalThis as typeof globalThis & { KTX2DECODER?: typeof KTX2DECODER }).KTX2DECODER = KTX2DECODER
+  KhronosTextureContainer2.DefaultNumWorkers = 0
+}
+
+configureLocalKtx2Decoder()
 
 const app = document.querySelector<HTMLDivElement>('#app')
 
